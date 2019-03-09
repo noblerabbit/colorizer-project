2ece9871f09a91db2d7f39c4565364cae28c4ffe
Igor <igor@cyftb.net>
2019-03-09 14:28:29 +0100

    simple  keras cnn  model and some structrue

---
+++ colorizer/networks/simple_cnn.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, Input, Lambda, MaxPooling2D, BatchNormalization, UpSampling2D 
+from tensorflow.keras.models import Sequential, Model
+
+
+
+def simple_cnn() -> Model:
+    encoder_input = Input(shape=(256, 256, 1,))
+    encoder_output = Conv2D(64, (3,3), activation='relu', padding='same', strides=2)(encoder_input)
+    encoder_output = BatchNormalization()(encoder_output)
+    encoder_output = Conv2D(128, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(128, (3,3), activation='relu', padding='same', strides=2)(encoder_output)
+    encoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(256, (3,3), activation='relu', padding='same', strides=2)(encoder_output)
+    encoder_output = Conv2D(512, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+    
+    #Decoder
+    decoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = Conv2D(128, (3,3), activation='relu', padding='same')(decoder_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = Conv2D(64, (3,3), activation='relu', padding='same')(decoder_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = Conv2D(32, (3,3), activation='relu', padding='same')(decoder_output)
+    decoder_output = Conv2D(2, (3, 3), activation='tanh', padding='same')(decoder_output)
+        
+    model = Model(inputs=encoder_input, outputs=decoder_output)
+    
+    return model;
